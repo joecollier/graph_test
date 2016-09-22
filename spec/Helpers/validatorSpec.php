b93b57ba0e81f3cc36d202b5c623f9a826616d1b
Your Name <you@example.com>
2016-09-22 15:42:20 -0400

    Updating class names

--- spec/Helpers/validatorSpec.php
+++ spec/Helpers/validatorSpec.php
@@ -6,7 +6,7 @@ use Editor\Helpers\validator;
 
 describe('validator', function () {
     beforeEach(function () {
-        $this->validateParams = Stub::create([
+        $this->ValidateParams = Stub::create([
             'extends' => validator::class,
             'params' => []
         ]);
@@ -14,20 +14,20 @@ describe('validator', function () {
 
     describe('->isValidDimensions()', function () {
         it('Returns true when x and y values are greater than 1 and lte 250', function () {
-            expect($this->validateParams->isValidDimensions(100,100))->toBe(true);
+            expect($this->ValidateParams->isValidDimensions(100,100))->toBe(true);
         });
 
         it(
             'Returns false when x value is greater than 1 and lte 250 and y is greater than 250',
             function () {
-                expect($this->validateParams->isValidDimensions(100,300))->toBe(false);
+                expect($this->ValidateParams->isValidDimensions(100,300))->toBe(false);
             }
         );
 
         it(
             'Returns false when x value is less than 1 and y value is greater than 1 and lte 250',
             function () {
-                expect($this->validateParams->isValidDimensions(0,100))->toBe(false);
+                expect($this->ValidateParams->isValidDimensions(0,100))->toBe(false);
             }
         );
     });
